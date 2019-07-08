21d0dc7981ec69956066867a1ad5066be539f02f
yeying <yeying0827@126.com>
2019-07-08 16:55:37 +0800

    history step add sort function; add dynamic className

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ class Board extends React.Component {
             <Square 
                 value={this.props.squares[i]} 
                 onClick={() => this.props.onClick(i)}
+                key={i}
             />
         );
     }
@@ -31,7 +32,7 @@ class Board extends React.Component {
                 {
                     arrays.map((item, index) => {
                         return (
-                            <div className="board-row">
+                            <div className="board-row" key={item}>
                                 {
                                     arrays.map((subitem, subindex) => {
                                         return (this.renderSquare(3*index + subindex))
@@ -59,12 +60,13 @@ class Game extends React.Component {
             }],
             stepNumber: 0,
             xIsNext: true,
+            historySort: 0, // 默认升序：0-升序 1-降序
         }
     }
 
     handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
+        const history = this.state.historySort === 0? this.state.history.slice(0, this.state.stepNumber + 1): this.state.history.slice(this.state.stepNumber);
+        const current = this.state.historySort === 0? history[history.length - 1]: history[0];
         const squares = current.squares.slice(); 
         if(calculateWinner(squares) || squares[i]) {
             return;
@@ -73,23 +75,55 @@ class Game extends React.Component {
         // 使用新数据替换旧数据 -> 撤销和恢复; 跟踪数据改变; 在React中创建pure components，确定何时重新渲染shouldComponentUpdate()
         squares[i] = this.state.xIsNext? 'X': 'O';
         this.setState({
-            history: history.concat([{
-                squares: squares,
-                position: {
-                    x: i % 3,
-                    y: Math.floor(i / 3)
-                },
-            }]),
-            stepNumber: history.length,
+            history: this.state.historySort === 0? 
+                history.concat([{
+                    squares: squares,
+                    position: {
+                        x: i % 3,
+                        y: Math.floor(i / 3)
+                    },
+                }]): [{
+                    squares: squares,
+                    position: {
+                        x: i % 3,
+                        y: Math.floor(i / 3)
+                    },
+                }].concat(history),
+            stepNumber: this.state.historySort === 0? history.length: 0,
             xIsNext: !this.state.xIsNext,
         });
+    }
+
+    /**
+     * update: is X the next player
+     * @return true / false
+     */
+    changePlayer(step, sort) {
+        const historyLength = this.state.history.length;
+        let flag = true;
+        if (sort === 0) {
+            flag = (step % 2) === 0
+        } else if (sort === 1) {
+            flag = (historyLength - 1 - step) % 2 === 0
+        }
+        return flag;
     }
 
     jumpTo(step) {
         this.setState({
             stepNumber: step,
-            xIsNext: (step % 2) === 0
+            xIsNext: this.changePlayer(step, this.state.historySort)
         })
+    }
+
+    changeHistorySort() {
+        let history = [...this.state.history];
+        history = history.reverse();
+        this.setState({
+            historySort: this.state.historySort? 0: 1, // change sort
+            history: history, // reverse array
+            xIsNext: this.changePlayer(this.state.stepNumber, this.state.historySort? 0: 1)
+        });
     }
 
     render() {
@@ -98,14 +132,16 @@ class Game extends React.Component {
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
-            const desc = move ?
-                'go to move #' + move + '(' + step.position.x + ',' + step.position.y + ')':
-                'go to start';
+            let desc = (this.state.historySort === 0 && move) || (this.state.historySort === 1 && move < history.length-1) ?
+                    'go to move #' + move + '(' + step.position.x + ',' + step.position.y + ')':
+                    'go to start';
+            
             
             return (
                 <li key={move}>
                     {
                         <button 
+                            className={"a"+(move === this.state.stepNumber?"1":"0")}
                             style={move === this.state.stepNumber?{fontWeight: 'bold'}:{}} 
                             onClick={() => this.jumpTo(move)}
                         >{desc}</button>
@@ -136,7 +172,13 @@ class Game extends React.Component {
                     />
                 </div>
                 <div className="game-info">
-                    <div>{status}</div>
+                    <div>{status} 
+                        <button 
+                            onClick={() => this.changeHistorySort()}
+                        >
+                            {this.state.historySort? 'Desc': 'Asc'}
+                        </button>
+                    </div>
                     <ol>{moves}</ol>
                 </div>
             </div>
